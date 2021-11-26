9b232f054f7f8130106fda62c81b0787a427458f
lriccotti <nayy4033@gmail.com>
2021-11-26 15:26:40 -0300

    a

---
+++ Image/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 16/11/2021
+ * Time: 12:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Image
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.btn1 = new System.Windows.Forms.Button();
+			this.pic1 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btn1
+			// 
+			this.btn1.Location = new System.Drawing.Point(68, 133);
+			this.btn1.Name = "btn1";
+			this.btn1.Size = new System.Drawing.Size(135, 71);
+			this.btn1.TabIndex = 0;
+			this.btn1.Text = "mostrar";
+			this.btn1.UseVisualStyleBackColor = true;
+			this.btn1.Click += new System.EventHandler(this.Btn1Click);
+			// 
+			// pic1
+			// 
+			this.pic1.Image = ((System.Drawing.Image)(resources.GetObject("pic1.Image")));
+			this.pic1.Location = new System.Drawing.Point(68, 12);
+			this.pic1.Name = "pic1";
+			this.pic1.Size = new System.Drawing.Size(135, 115);
+			this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pic1.TabIndex = 1;
+			this.pic1.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.pic1);
+			this.Controls.Add(this.btn1);
+			this.Name = "MainForm";
+			this.Text = "Image";
+			((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+			this.ResumeLayout(false);
+		}
+		private System.Windows.Forms.PictureBox pic1;
+		private System.Windows.Forms.Button btn1;
+	}
+}
---
+++ Image/MainForm.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: VaCkensi
+ * Date: 16/11/2021
+ * Time: 12:16
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Image
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Btn1Click(object sender, EventArgs e)
+		{
+			if (pic1.Visible==false) {
+				pic1.Visible=true;
+			}else{
+				pic1.Visible=false;
+			}
+		}
+	}
+}
